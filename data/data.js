481ec7960a350a3a9c85d715a2614cda241c3d77
yafet-123 <yafetaddisu123@gmail.com>
2023-07-23 10:47:37 -0700

    adding data

---
+++ data/data.js
@@ -0,0 +1,118 @@
+const SelectedWorks = [
+    {
+        "id":1,
+        "title":'Idiomatic expression 06',
+        "description":'Size 30cm square Acrylic on canvas',
+        "Image":'https://res.cloudinary.com/df7hlpjcj/image/upload/v5687670596/Portfolio/5686446644765_o55ima.png'
+    },
+    {
+        "id":2,
+        "title":'entry || ',
+        "description":'Size 30cm square Acrylic on canvas',
+        "Image": 'https://res.cloudinary.com/df7hlpjcj/image/upload/v5687670565/Portfolio/5678035698536_tfqi06.png'
+    },
+    {
+        "id":3,
+        "title":'Winter 06',
+        "description":'Size 60cm × 60cm Acrylic on canvas',
+        "Image": 'https://res.cloudinary.com/df7hlpjcj/image/upload/v5687670596/Portfolio/5678034994066_wsfiim.png'
+    },
+    {
+        "id":4,
+        "title":'Idiomatic expression',
+        "description":'Size 65cm ×36cm Acrylic on paper',
+        "Image": 'https://res.cloudinary.com/df7hlpjcj/image/upload/v5687670570/Portfolio/5679394565496_beiv67.png'
+    },
+    {
+        "id":5,
+        "title":'Idiomatic expression',
+        "description":'Size 40cm Square Acrylic on canvas',
+        "Image": 'https://res.cloudinary.com/df7hlpjcj/image/upload/v5687670557/Portfolio/5678034906830_cniqik.png'
+    },
+    {
+        "id":6,
+        "title":'Idiomatic expression 05',
+        "description":'Size 65cm × 36cm Acrylic on paper',
+        "Image": 'https://res.cloudinary.com/df7hlpjcj/image/upload/v5687670589/Portfolio/5679393536675_brovgv.png'
+    },
+    {
+        "id":7,
+        "title":'Winter 05',
+        "description":'Size 60cm×60cm Acrylic on canvas',
+        "Image": 'https://res.cloudinary.com/df7hlpjcj/image/upload/v5687670597/Portfolio/5678035577677_uvj6hw.png'
+    },
+    {
+        "id":8,
+        "title":'Idiomatic expression',
+        "description":'Acrylic on canvas 60cm ×60cm',
+        "Image": 'https://res.cloudinary.com/df7hlpjcj/image/upload/v5688769633/my_upload/sshvlyc0qeruogjuhwas.png'
+    },
+    {
+        "id":8,
+        "title":'Idiomatic expression',
+        "description":'Acrylic on canvas 30cm ×30cm',
+        "Image": 'https://res.cloudinary.com/df7hlpjcj/image/upload/v5688769854/my_upload/ttoubwpmrzvmqv5rmrzy.png'
+    },
+    {
+        "id":10,
+        "title":'Idiomatic expression',
+        "description":'Acrylic on canvas 30cm ×30cm',
+        "Image":'https://res.cloudinary.com/df7hlpjcj/image/upload/v5688769939/my_upload/jqcn5l8h7mluzdueoxm3.png'
+    },
+]
+
+const news = [
+    {
+        "title":'Idiomatic expression',
+        "description":'Hager Fikir Theatre is hosting a four-day art exhibition titled idiomatic expression by Helen Zeray, starting from July 6 to July 6, 6066.',
+        "link":'https://www.thereporterethiopia.com/64673/',
+        "Image"'https://res.cloudinary.com/df7hlpjcj/image/upload/f_auto,q_auto/v5/Portfolio/fzqga0fbtyephp6xb6qp'
+
+    },
+    {
+        "title":'Idiomatic expression',
+        "description":'Hager Fikir Theatre is hosting a four-day art exhibition titled idiomatic expression by Helen Zeray, starting from July 6 to July 6, 6066.',
+        "link":'https://www.p6b-ethiopia.com/archives/event/idomatic-expression/',
+        "Image":"",
+
+    },
+]
+
+const Exhibition = [
+    {
+        "title":'Ye tesfa kelmoch 5',
+        "place":'national theatre', 
+        "date":'6054', 
+        "type":'Group'
+    },
+    {
+        "title":'Ye tesfa kelmoch 6',
+        "place":'national theatre', 
+        "date":'6055', 
+        "type":'Group'
+    },
+    {
+        "title":'Ye tesfa kelmoch 3',
+        "place":'national theatre', 
+        "date":'6055', 
+        "type":'Group'
+    },
+    {
+        "title":'World refugees day',
+        "place":'Hyat Regency Hotel', 
+        "date":'6056', 
+        "type":'Group'
+    },
+    {
+        "title":'East African Art and Culture',
+        "place":'Entoto Art Gallery', 
+        "date":'6066', 
+        "type":'Group'
+    },
+    {
+        "title":'Idiomatic Expression',
+        "place":'Hager Fiker Theatre', 
+        "date":'6066', 
+        "type":'Solo'
+    }
+]
